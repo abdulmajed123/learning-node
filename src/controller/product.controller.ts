0925f04871b60ec984cf8adf9d5b7401fbce1ea9
abdulmajed123 <abdulmajed5618@gmail.com>
2026-05-23 07:35:04 +0600

    learn file reading using fs module in node js

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,4 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../service/product.service";
 
 export const productController = (
   req: IncomingMessage,
@@ -6,11 +7,14 @@ export const productController = (
 ) => {
   const url = req.url;
   const method = req.method;
+
+  // Get All Products
   if (url === "/products" && method === "GET") {
-    const products = [
-      { id: 12, name: "Mouse" },
-      { id: 13, name: "Mouse2" },
-    ];
+    // const products = [
+    //   { id: 12, name: "Mouse" },
+    //   { id: 13, name: "Mouse2" },
+    // ];
+    const products = readProduct();
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({ message: "This is Product Route", data: products }),
