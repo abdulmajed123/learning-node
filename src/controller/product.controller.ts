8106cb72d8f44a524229a7dde219448e48ee8b38
abdulmajed123 <abdulmajed5618@gmail.com>
2026-05-24 10:04:58 +0600

    learn parsing request body in node js

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,12 +1,18 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { readProduct } from "../service/product.service";
+import type { Iproduct } from "../types/product.type";
+import { parseBody } from "../utility/parseBodey";
 
-export const productController = (
+export const productController = async (
   req: IncomingMessage,
   res: ServerResponse,
 ) => {
   const url = req.url;
   const method = req.method;
+  const urlParts = url?.split("/");
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+  console.log(id);
 
   // Get All Products
   if (url === "/products" && method === "GET") {
@@ -17,7 +23,31 @@ export const productController = (
     const products = readProduct();
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
-      JSON.stringify({ message: "This is Product Route", data: products }),
+      JSON.stringify({
+        message: "Products retrived successfully",
+        data: products,
+      }),
+    );
+  } else if (method === "GET" && id !== null) {
+    // Get Single Product
+    const products = readProduct();
+    const product = products.find((p: Iproduct) => p.id === id);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Products retrived successfully",
+        data: product,
+      }),
+    );
+  } else if (method === "POST" && url === "/products") {
+    const body = await parseBody(req);
+    console.log(body);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Products create successfully",
+        // data: product,
+      }),
     );
   }
 };
